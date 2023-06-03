624e4cd9257d78c44537960815c6e9e825f255b8
saurav-2k01 <saurav2309.s@outlook.com>
2023-06-03 18:01:41 +0530

    Added some features, to test functions in bulk and visualize them for easy comparison.

--- Samay/Samay.py
+++ Samay/Samay.py
@@ -1,57 +1,40 @@
-# Importing requried Modules and packages
 import time
+import matplotlib.pyplot as plt
 
-
-# Samay is a Module to compare execution time of two functions.
-
-
-# Creating class Samay
-class samay:
-    """
-        Creating a Samay class constructor having following attributes.
-        func = func is an attribute to parse a function.
-        args = args is a tuple containing all parameters of the function that has been parsed.
+class Samay:
 
     """
+            Samay class is used to find the execution time of function and compare execution time of those function.
+            func: func is an attribute to parse a function.
+            function_name: function_name holds the Temporary name for the initialized objects.
+            args: args is a tuple containing all parameters of the function that has been parsed.
+            id: unique id is generated everytime for a new instance.
+    """
 
-    def __init__(self, func, args=()):
-        self._func = func
-        self._args = args
-        self._loop = 1  # _loop holds the number of loops to increase execution time of a function, _loop is equal to 1 by default.
-        self._object_name = None  # _object_name is hold the Temporary name for the initialized objects.
-
-    # Defining object_name as a property of a function.
-    @property
-    def object_name(self):
-        return self._object_name
-
-    # Setting object_name
-    @object_name.setter
-    def object_name(self, object=str):
-        if object == None:
-            return f'Objects are set to None'
+    object_count = 0
+    def __init__(self, func, function_name:str=None, args=()):
+        Samay.object_count += 1
+        self.id = Samay.object_count
+        self.func = func
+        self.function_name = function_name
+        if self.function_name:
+            pass
         else:
-            self._object_name = object
+            self.function_name = f"function {self.id}"
+        self.args = args
 
-    # Defining Loop as a property.
-    @property
-    def loop(self):
-        return self._loop
 
-    # Setting loop as a property.
-    @loop.setter
-    def loop(self, loops):
-        if loops == 1:
-            return None
-        else:
-            self._loop = loops
+    def get_exe_time(self, loops:int=10):
 
-    # Creating a  function that calculates the execution time of a function.
-    def get_exe_time(self):
-        if self._func != None:
+        """
+            loops holds the number of loops to increase execution time of a function,
+            loops is equal to 1 by default.
+        """
+
+        if self.func != None:
             start = time.time()
-            for i in range(self._loop):
-                self._func(*self._args)
+            for i in range(loops):
+                self.func(*self.args)
             end = time.time()
             return end - start
         else:
@@ -59,24 +42,87 @@ class samay:
 
     # Creating a function that compares two different function.
     def compare_function(self, other):
+
+        """
+            compare_function is used to compare two different function by the difference between
+            the execution time of those two function.
+        """
+
         function_1 = self.get_exe_time()
         function_2 = other.get_exe_time()
         difference = function_1 - function_2
 
-        """ 
-            if object_name are not set, then it only returns the difference.
-            else if Object_name are set, then it returns execution time of both 
-            the functions as well as differnce between them. 
-
-        """
-
-        if self.object_name == None or other._object_name == None:
+        if self.function_name == None or other.function_name == None:
             return f'Difference  : {difference}'
         else:
             return {
-                self._object_name: function_1,
-                other._object_name: function_2,
+                self.function_name: function_1,
+                other.function_name: function_2,
                 'Difference': difference
             }
 
 
+class Samay_Bulk:
+
+    """ Samay_Bulk class is used to test and compare function in bulk at once.
+        functions: functions parameters takes a list of tuples containg function, function_name and args
+        example:
+            data = [
+                (func1, "function1", (x, y)),
+                (func2, (x, y)),
+                (func3,)
+            ]
+        result: result holds the execution time as output with their function
+        example:
+            {"function1":0.0, "function2":0.0, "function3":0.0 }
+    """
+
+    def __init__(self, functions:list=[]):
+        self.functions = functions
+        self.function_count = len(self.functions)
+        self.result = {}
+
+
+    def filter(self,data):
+        types = [type(x) for x in data]
+        if str in types and tuple in types:
+            return str, tuple
+        elif str in types:
+            return str
+        elif tuple in types:
+            return tuple
+
+
+    def test_bulk(self, loops:int=10):
+
+        """ test_bulk function is used to find the exection time of function in bulk,
+            more than 2 function could be tested, and the result is stored in dictionary called
+            result.
+            example:
+                temp = Samay_Bulk(data)
+                temp.test_bulk()
+                result = temp.result
+        """
+
+        for function in self.functions:
+            if self.filter(function) == (str, tuple):
+                process = Samay(func=function[0], function_name=function[1], args=function[2])
+                self.result[process.function_name] = process.get_exe_time(loops)
+            elif self.filter(function) == tuple:
+                process = Samay(func=function[0], args=function[1])
+                self.result[process.function_name] = process.get_exe_time(loops)
+            elif self.filter(function) == str:
+                process = Samay(func=function[0], function_name=function[1])
+                self.result[process.function_name] = process.get_exe_time(loops)
+            else:
+                process = Samay(func=function[0])
+                self.result[process.function_name] = process.get_exe_time(loops)
+
+
+    def bar_chart(self):
+        """ bar_chart function is used to visualize the result for easy comparison. """
+        plt.bar(self.result.keys(), self.result.values())
+        plt.title("Bar Chart")
+        plt.xlabel("Function")
+        plt.ylabel("Time")
+        plt.show()
